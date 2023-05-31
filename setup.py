5125e8e630dd6d45530318c3a004ac445e570e2e
scottyler892 <scottyler89+bitbucket@gmail.com>
2023-05-31 08:42:05 -0400

    fixed setup bug with braces

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     extras_require={
         'docs': [
             'm2r2'
-        ],
+        ]},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: GNU Affero General Public License v3",
